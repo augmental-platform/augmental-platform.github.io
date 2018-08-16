559d8c81ff7e828c9575513aa44d4be3cc3addbe
Hendrik Schalekamp <carnun@sudo.org.za>
2018-08-16 22:57:41 +0200

    added link page to the github org

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to our new Gatsby site.</p>
-    <p>Now we need to go build something great.</p>
+    <p>Now we need to go build something <a href="https://github.com/augmental-platform/">great</a>.</p>
   </div>
 )
 
